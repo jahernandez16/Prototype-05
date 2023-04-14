4481c1a3048c31914112d8d49358b9d347ac879a
Jasmin <122053640+jahernandez16@users.noreply.github.com>
2023-04-14 09:02:06 -0700

    5.3
    Did not finish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,8 +17,6 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
-
-        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -41,5 +39,10 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score:" + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
